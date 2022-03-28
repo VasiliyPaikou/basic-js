405a8c3990d24d7e6d1724637007157e557edac2
VasiliyPaikou <gtr208111@gmail.com>
2022-03-28 13:45:41 +0300

    feat: what season, transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+	if (!(arr instanceof Array)) {
+		throw  Error("'arr' parameter must be an instance of the Array!");
+	}
+	if (arr.length === 0) {
+		return []
+	}
+	let newArr = arr.slice()
+	if (arr.indexOf('--discard-next') > -1) {
+		if (newArr.indexOf('--discard-next') == newArr.length - 1) {
+			newArr.splice(newArr.indexOf('--discard-next'), 1)
+		} else {
+			newArr.splice(newArr.indexOf('--discard-next'), 2, ' ')
+		}
+
+	}
+	if (arr.indexOf('--double-next') > -1) {
+		if (newArr.indexOf('--double-next') == newArr.length - 1) {
+			newArr.splice(newArr.indexOf('--double-next'), 1)
+		} else {
+			newArr.splice(newArr.indexOf('--double-next'), 1, newArr[newArr.indexOf('--double-next') + 1])
+		}
+	}
+	if (arr.indexOf('--discard-prev') > -1) {
+		if (newArr.indexOf('--discard-prev') === 0) {
+			newArr.splice(newArr.indexOf('--discard-prev'), 1)
+		} else {
+			newArr.splice(newArr.indexOf('--discard-prev') - 1, 2)
+		}
+	}
+	if (newArr.indexOf('--double-prev') > -1) {
+		if (newArr.indexOf('--double-prev') === 0) {
+			newArr.splice(newArr.indexOf('--double-prev'), 1)
+		} else {
+			newArr.splice(newArr.indexOf('--double-prev'), 1, newArr[newArr.indexOf('--double-prev') - 1])
+		}
+	}
+	return newArr.filter(a => a !== ' ')
 }
 
 module.exports = {
-  transform
+	transform
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date){
+if (date === undefined) {
+	return 'Unable to determine the time of year!'
 }
+// if (isNaN(Date.parse(date))) {
+// 	throw new Error('Invalid date!');
+// 	}
+if (!(date instanceof Date)) {
+	throw new Error('Invalid date!');
+}
+// if (!(Object.prototype.toString.call(date) == '[object Date]')) {
+// 	throw new Error('Invalid date!');
+// }
+// if (!(date.getTime)) {
+// 	throw new Error('Invalid date!');
+	
+// }
+if ( Object.keys(date).length > 0 ) {
+	throw new Error('Invalid date!');
+}
+	let seasons = {
+	'winter': [0,1,11],
+	'spring': [2,3,4],
+	'summer': [5,6,7],
+	'autumn': [8,9,10],
+	}
+	let date1 = new Date(date).getMonth()
+	for (const key in seasons) {
+	if (seasons[key].includes(date1)) {
+	return key
+	}
+	}
+	}
 
 module.exports = {
   getSeason
