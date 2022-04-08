326c953e00eb6df913d4ae851afaa3139ef8a4ae
VasiliyPaikou <gtr208111@gmail.com>
2022-04-08 06:16:21 +0300

    feat:extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	let intro = options
+	const subOject = {
+		stack: [],
+		subStack: [],
+		r: intro.repeatTimes,
+		v: intro.additionRepeatTimes,
+		repeatTimes() {
+			typeof str !== 'string' && typeof intro.addition !== 'string' ? ((str = String(str)), (intro.addition = String(intro.addition))) : ((str = str), (intro.addition = intro.addition))
+			intro.addition !== undefined ? add = intro.addition : add = ''
+			intro.additionSeparator !== undefined ? der = intro.additionSeparator : der = '|'
+			intro.separator !== undefined ? sep = intro.separator : sep = '+'
+			intro.additionRepeatTimes === undefined ? this.v = 1 : this.v = intro.additionRepeatTimes
+			intro.repeatTimes === undefined ? this.r = 1 : this.r = intro.repeatTimes
+			let y = 0
+			let i = 0
+			while (y < this.v) {
+				this.subStack.push(add)
+				this.subStack.push(der)
+				y++
+			}
+			this.subStack.pop()
+			while (i < this.r) {
+				this.stack.push(str)
+				this.stack.push(this.subStack.join(''))
+				this.stack.push(sep)
+				i++
+			}
+			this.stack.pop()
+			return this.stack.join('')
+		}
+	}
+	return subOject.repeatTimes()
 }
 
 module.exports = {
-  repeater
+	repeater
 };
