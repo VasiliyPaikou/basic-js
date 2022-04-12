693260329355b1e4c2f860f524b0a07f4e4db2d0
VasiliyPaikou <gtr208111@gmail.com>
2022-04-12 11:18:18 +0300

    feat:finish task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -29,3 +29,4 @@ class DepthCalculator {
 module.exports = {
 	DepthCalculator
 };
+
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,17 +19,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
-
+ class VigenereCipheringMachine {
+	constructor(flag = true){
+	this.reverseflaf = flag
+	}
+	encrypt(string, key) {
+	if (string === undefined || key === undefined ) {
+	throw new Error('Incorrect arguments!')
+	}
+	let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let one = string.toUpperCase().split('')
+	let ghg = key.toUpperCase().split('')
+	let keyS = []
+	let num = ''
+	if (one.length > keyS.length) {
+	for (let i = 0; i < one.length; i++) {
+	ghg.length === 0 ? ghg = key.toUpperCase().split('') : ghg = ghg
+	alpha.indexOf(one[i]) !== -1 ? (keyS.push(ghg[0]), ghg.shift()) : keyS.push(one[i])
+	}
+	}
+	for (let i = 0; i < one.length; i++) {
+	if (alpha.indexOf(one[i]) === -1) {
+	num+=one[i]
+	}else{
+	if (alpha.indexOf(one[i]) + alpha.indexOf(keyS.join('')[i])>= alpha.length) {
+	num += alpha[Math.abs((alpha.indexOf(one[i]) + alpha.indexOf(keyS.join('')[i]) - alpha.length))]
+	}else{
+	num += alpha[(alpha.indexOf(one[i]) + alpha.indexOf(keyS.join('')[i]))]
+	}
+	}
+	}
+	if (this.reverseflaf === false) return num.split('').reverse().join('')
+	return num
+	}
+	decrypt(string, key) {
+	if (string === undefined || key === undefined ) {
+	throw new Error('Incorrect arguments!')
+	}
+	let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let one = string.toUpperCase().split('')
+	let ghg = key.toUpperCase().split('')
+	let keyS = []
+	let num = ''
+	if (one.length > keyS.length) {
+	for (let i = 0; i < one.length; i++) {
+	ghg.length === 0 ? ghg = key.toUpperCase().split('') : ghg = ghg
+	alpha.indexOf(one[i]) !== -1 ? (keyS.push(ghg[0]), ghg.shift()) : keyS.push(one[i])
+	}
+	}
+	for (let i = 0; i < one.length; i++) {
+	if (alpha.indexOf(one[i]) === -1) {
+	num+=one[i]
+	}else{
+	if (alpha.indexOf(one[i]) - alpha.indexOf(keyS.join('')[i]) < 0) {
+	num += alpha[Math.abs((alpha.indexOf(one[i]) + alpha.length - alpha.indexOf(keyS.join('')[i])))]
+	}else{
+	num += alpha[(alpha.indexOf(one[i]) - alpha.indexOf(keyS.join('')[i]))]
+	}
+	}
+	}
+	if (this.reverseflaf === false) return num.split('').reverse().join('')
+	return num
+	}
+	}
+	
 module.exports = {
   VigenereCipheringMachine
 };
